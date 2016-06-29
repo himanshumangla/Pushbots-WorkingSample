ab599609df1ace00f63f542042890eaebbbd420d
Himanshu Mangla <him.mangla@gmail.com>
2016-06-29 13:05:08 -0400

    Modification

--- app/src/main/java/com/test/blankproject/MainActivity.java
+++ app/src/main/java/com/test/blankproject/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "This button doesn't do anything yet..! ", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
